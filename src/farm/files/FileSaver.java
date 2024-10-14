2232f112896e380cafed71de5c275db897f3f848
KoroSyndrome <ash10@hotmail.com.au>
2024-10-15 09:27:05 +1000

    Add files via upload

---
+++ src/farm/files/FileSaver.java
@@ -0,0 +1,24 @@
+package farm.files;
+
+import farm.core.farmgrid.Grid;
+import java.io.IOException;
+
+public class FileSaver {
+
+    /**
+     * Constructor for the FileSaver class.
+     */
+    public FileSaver() {
+
+    }
+
+    /**
+     * Saves the contents of the provided grid to a file with specified name.
+     * @param filename the path of the save file to create
+     * @param grid the grid instance to save to a file
+     * @throws IOException iff an error occurred when trying to write the file.
+     */
+    public void save(String filename, Grid grid) throws IOException {
+
+    }
+}
